c0bc7175764468babecfbc72a0f1d22977fc82c9
JoeComputational <userade11@gmail.com>
2022-03-08 14:45:41 +1000

    Built the URLS but also imported the HTML for index page

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Category, Product
 
-# Create your views here.
+def all_products(request):
+    products = Product.objects.all()
+    return render(request, 'store/index.html', {'products': products})
